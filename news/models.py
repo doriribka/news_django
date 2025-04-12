10f7667c85c3ce85c3aaf35c98194d3015ae7b61
user <dzverkova50@gmail.com>
2025-04-13 02:23:37 +0300

    Обновлен models.py

--- news/models.py
+++ news/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 
-class Image_news(models.Model):
-    image = models.ImageField(blank=True, null=True, upload_to='Images_news')
-
-
 class News(models.Model):
     heading_news = models.TextField()
     date_news = models.DateField()
     short_news = models.TextField()
     text_long_news = models.TextField()
     pin = models.FileField(blank=True, null=True)
-    image = models.ManyToManyField(Image_newsв )
+
+
+class ImageNews(models.Model):
+    image = models.ImageField(blank=True, null=True, upload_to='ImagesNews')
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='images')
